f2dd3618ad42aed9d610a07aa743a0be64059cad
Erfan Seidipoor <erfan.seidipoor@gmail.com>
2023-07-22 17:45:39 +0330

    Initial Setup: Frontend Project with React & Material UI

---
+++ apps/front/src/app/app.tsx
@@ -0,0 +1,14 @@
+import './app.css';
+
+import Button from '@mui/material/Button';
+
+export function App() {
+  return (
+    <div>
+      <h1>{'ottonova'}</h1>
+      <Button>{'ottonova'}</Button>
+    </div>
+  );
+}
+
+export default App;
